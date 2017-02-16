50be9210b6609f382e203a6de98bd02f5193dd68
薛依玲 <wb-xyl259837@alibaba-inc.com>
2017-02-16 17:41:34 +0800

    v2017.0216.174018

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ BRANCH=`date +%Y.%m%d.%H%M%S`
 npm run build
 git commit -am v$BRANCH
 git push origin source
-cp -R build ~/.salt
-git checkout master
-cp -R ~/.salt/build/* ./ 
-rm -rf ~/.salt/
+# cp -R build ~/.salt
+# git checkout master
+# cp -R ~/.salt/build/* ./ 
+# rm -rf ~/.salt/
 # git commit -m v$BRANCH
 # git push origin master
 # git checkout source
