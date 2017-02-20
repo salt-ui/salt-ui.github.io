22fb551d1590b91d0db01d26774ce250a18cda5e
gbk <ck0123456@gmail.com>
2017-02-20 12:48:48 +0800

    v2017.0220.124808

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,8 @@ mkdir ~/SALT_UI_BUILD
 mv ./build/* ~/SALT_UI_BUILD/
 git checkout master
 cp -rf ~/SALT_UI_BUILD/* .
-# rm -rf ~/SALT_UI_BUILD
-# git add .
-# git commit -m v$BRANCH
-# git push origin master
-# git checkout source
-
+rm -rf ~/SALT_UI_BUILD
+git add .
+git commit -m v$BRANCH
+git push origin master
+git checkout source
