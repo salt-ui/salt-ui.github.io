b561c08bc254c47b8e8b17d2a453fd2c597f0f69
gbk <ck0123456@gmail.com>
2017-02-20 12:31:05 +0800

    v2017.0220.123105

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ npm run build
 git commit -am v$BRANCH
 git push origin source
 mv build ~/.salt
-git checkout master
-rm -rf *
-mv ~/.salt/* .
+# git checkout master
+# rm -rf *
+# mv ~/.salt/* .
 # git add .
 # git commit -m v$BRANCH
 # git push origin master
