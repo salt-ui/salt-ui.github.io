e7eb2a8d4632cf8d72635a86ec43f1e4874f2c06
gbk <ck0123456@gmail.com>
2017-02-20 12:45:28 +0800

    v2017.0220.124449

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@ BRANCH=`date +%Y.%m%d.%H%M%S`
 npm run build
 git commit -am v$BRANCH
 git push origin source
-rm -rf ~/.salt
-mv build/* ~/.salt
+mkdir ~/SALT_UI_BUILD
+mv ./build/* ~/SALT_UI_BUILD/
 git checkout master
-cp -rf ~/.salt/* .
+cp -rf ~/SALT_UI_BUILD/* .
+# rm -rf ~/SALT_UI_BUILD
 # git add .
 # git commit -m v$BRANCH
 # git push origin master
