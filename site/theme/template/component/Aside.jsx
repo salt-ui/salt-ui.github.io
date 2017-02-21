825ca6043f0d1d34089516b8a4693834050b0277
Jirapo <466680197@qq.com>
2017-02-21 10:50:45 +0800

    add classify to aside

--- site/theme/template/component/Aside.jsx
+++ site/theme/template/component/Aside.jsx
@@ -5,43 +5,40 @@ import {upperFirstCharactor, removeTingle, mappingNavItem } from '../../../utils
 import { typeOrder } from '../../../constants';
 
 
-
 export default ({ sideNav, components, params, types }) => {
-
-  const prt = typeOrder.map(type => {
-    if(types[type]){
-      return (
-          <li className="aside-menu-group">
-            <div className="aside-menu-group-title">{type}</div>
-            <ul className="aside-menu-group-list">
-              {
-                types[type].map(list => 
-                  <li 
-                    key={list.name}
-                    className={classnames({
-                      'aside-menu-item': true,
-                      active: params.component === list.name,
-                    })}> 
-                    <Link to={`/components/${list.name}`}>
-                      { upperFirstCharactor(removeTingle(list.name))}
-                      <span>{ list.zh}</span>
-                    </Link>
-                  </li>
-                )
-              }
-            </ul>
-          </li>
-        );
-    }else{
-      return null;
-    }
-  })
 
   return (
     <div className="aside">
       <ul className="aside-menu">
       { 
-        prt
+        typeOrder.map(type => {
+          if(types[type]){
+            return (
+                <li className="aside-menu-group">
+                  <div className="aside-menu-group-title">{type}</div>
+                  <ul className="aside-menu-group-list">
+                    {
+                      types[type].map(list => 
+                        <li 
+                          key={list.name}
+                          className={classnames({
+                            'aside-menu-item': true,
+                            active: params.component === list.name,
+                          })}> 
+                          <Link to={`/components/${list.name}`}>
+                            { upperFirstCharactor(removeTingle(list.name))}
+                            <span>{ list.zh}</span>
+                          </Link>
+                        </li>
+                      )
+                    }
+                  </ul>
+                </li>
+              );
+          }else{
+            return null;
+          }
+        })
       }
       </ul>
     </div>
