b2b916e7115857ed7f759abf0ce189779015ff4e
gbk <ck0123456@gmail.com>
2017-04-27 11:47:33 +0800

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-echo "Start to copy..."
+echo "Copy documents..."
 cd node_modules/@ali
 rsync -R tingle-*/README.md tingle-*/HISTORY.md tingle-*/package.json ../../components
 cd ../../
 for file in components/tingle-*/README.md; do mv -f "$file" "${file/README/index}"; done
 rm -f components/tingle-ui/index.md
-echo "Copy finished."
+echo "Update tingle-ui version..."
+TINGLE=`tnpm view @ali/tingle-ui version`
+OS=`uname`
+if [[ "$OS" == "Darwin" ]]; then
+  sed -i "" "s/tingle-ui\/[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/tingle-ui\/$TINGLE/g" template.mobile.html
+else
+  sed -i "s/tingle-ui\/[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*/tingle-ui\/$TINGLE/g" template.mobile.html
+fi
