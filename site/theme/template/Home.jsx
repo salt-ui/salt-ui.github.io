91a90f908a499987f86975680eba2303c0d61873
薛依玲 <wb-xyl259837@alibaba-inc.com>
2017-07-20 15:23:53 +0800

    v2017.0720.152305

--- site/theme/template/Home.jsx
+++ site/theme/template/Home.jsx
@@ -55,33 +55,7 @@ export default (props) => {
         </div>
         <div className="kuma-container-1180 layout-demo-container " style={{ position: 'relative' }}>
           <Row className="ui-home-feature">
-            <Col md={8}>
-              <div className="ui-home-feature-item">
-                <div className="ui-home-feature-body">  
-                  <img src={imgs[theme].feature1} className="ui-home-feature-img" />
-                  <h3>指南</h3>
-                  <div className="ui-home-feature-intro">了解设计指南，帮助产品设计人员搭建逻辑清晰、结构合理且高效易用的产品。</div>
-                </div>
-              </div>
-            </Col>
-            <Col md={8}>
-              <div className="ui-home-feature-item">
-                <div className="ui-home-feature-body">  
-                  <img src={imgs[theme].feature2} className="ui-home-feature-img" />
-                  <h3>组件</h3>
-                  <div className="ui-home-feature-intro">使用组件 Demo 快速体验交互细节；使用前端框架封装的代码帮助工程师快速开发。</div>
-                </div>
-              </div>
-            </Col>
-            <Col md={8}>
-              <div className="ui-home-feature-item">
-                <div className="ui-home-feature-body">  
-                  <img src={imgs[theme].feature3} className="ui-home-feature-img" />
-                  <h3>资源</h3>
-                  <div className="ui-home-feature-intro">下载相关资源，用其快速搭建页面原型或高保真视觉稿，提升产品设计效率。</div>
-                </div>
-              </div>
-            </Col>
+            
           </Row>
         </div>
         <Footer />
@@ -90,49 +64,34 @@ export default (props) => {
   )
 };
 
-// <Grid fluid={true}>
-//         <Row className="home-banner">
-//           <Col md={14} >
-//             <h1>高效、简洁的移动端 UI 组件库
-//             </h1>
-//             <p>钉钉官方合作</p>
-//             <a className="start-btn" href="/components/tingle-avatar">Get Started</a>
-//           </Col>
-//           <Col md={10} >
-//             <div className="bg-phone"></div>
-//           </Col>
-//         </Row>
-//         <Row className="site-feature">
-//           <Col md={8}>
-//             <div className="feature-item">
-//               <div className="feature-body">  
-//                 <div className="feature-img img-intro"></div>
-//                 <h3>指南</h3>
-//                 <div className="intro">了解设计指南，帮助产品设计人员搭建逻辑清晰、结构合理且高效易用的产品。</div>
-//               </div>
-//             </div>
-//           </Col>
-//           <Col md={8}>
-//             <div className="feature-item">
-//               <div className="feature-body">  
-//                 <div className="feature-img img-comp"></div>
-//                 <h3>组件</h3>
-//                 <div className="intro">使用组件 Demo 快速体验交互细节；使用前端框架封装的代码帮助工程师快速开发。</div>
-//               </div>
-//             </div>
-//           </Col>
-//           <Col md={8}>
-//             <div className="feature-item">
-//               <div className="feature-body">  
-//                 <div className="feature-img img-res"></div>
-//                 <h3>资源</h3>
-//                 <div className="intro">下载相关资源，用其快速搭建页面原型或高保真视觉稿，提升产品设计效率。</div>
-//               </div>
-//             </div>
-//           </Col>
-//         </Row>
-//       </Grid>
-//       <Footer />
+// <Col md={8}>
+//   <div className="ui-home-feature-item">
+//     <div className="ui-home-feature-body">  
+//       <img src={imgs[theme].feature1} className="ui-home-feature-img" />
+//       <h3>指南</h3>
+//       <div className="ui-home-feature-intro">了解设计指南，帮助产品设计人员搭建逻辑清晰、结构合理且高效易用的产品。</div>
+//     </div>
+//   </div>
+// </Col>
+// <Col md={8}>
+//   <div className="ui-home-feature-item">
+//     <div className="ui-home-feature-body">  
+//       <img src={imgs[theme].feature2} className="ui-home-feature-img" />
+//       <h3>组件</h3>
+//       <div className="ui-home-feature-intro">使用组件 Demo 快速体验交互细节；使用前端框架封装的代码帮助工程师快速开发。</div>
+//     </div>
+//   </div>
+// </Col>
+// <Col md={8}>
+//   <div className="ui-home-feature-item">
+//     <div className="ui-home-feature-body">  
+//       <img src={imgs[theme].feature3} className="ui-home-feature-img" />
+//       <h3>资源</h3>
+//       <div className="ui-home-feature-intro">下载相关资源，用其快速搭建页面原型或高保真视觉稿，提升产品设计效率。</div>
+//     </div>
+//   </div>
+// </Col>
+
 
 
 
