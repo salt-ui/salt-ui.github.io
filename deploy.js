5bc7ac72e2032abc3791959adeca70c2f72b86ac
光弘 <guanghong.wsj@alibaba-inc.com>
2017-10-26 20:51:06 +0800

    v2017-10-26T12:49:56.567Z

--- deploy.js
+++ deploy.js
@@ -36,13 +36,13 @@ const deploy = (theme = 'blue') => {
   cmd('git add .');
   cmd(`git commit -m v${BRANCH}`);
   cmd('git push origin source-blue');
-  cmd('mkdir ~/SALT_UI_BUILD');
-  cmd('mv ./build/* ~/SALT_UI_BUILD/');
+  cmd('mkdir ./SALT_UI_BUILD');
+  cmd('mv ./build/* ./SALT_UI_BUILD/');
   cmd('git checkout master');
   cmd('git pull origin master');
-  cmd('cp -rf ~/SALT_UI_BUILD/* .');
-  cmd(`cp -rf ~/SALT_UI_BUILD/index.css ./${theme}.css`);
-  cmd('rm -rf ~/SALT_UI_BUILD');
+  cmd('cp -rf ./SALT_UI_BUILD/* .');
+  cmd(`cp -rf ./SALT_UI_BUILD/index.css ./${theme}.css`);
+  cmd('rm -rf ./SALT_UI_BUILD');
   cmd('git add .');
   cmd(`git commit -m v${BRANCH}`);
   cmd('git push origin master');
